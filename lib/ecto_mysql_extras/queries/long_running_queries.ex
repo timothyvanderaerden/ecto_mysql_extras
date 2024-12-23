08973f396f7b1ca85e574c9ed299855f0173d528
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2024-12-23 16:39:28 +0100

    Revert "Drop queries for MySQL `5.7`"
    This reverts commit 7b96186b0e1c542bf9505748271f3ec575e69fa1.

--- lib/ecto_mysql_extras/queries/long_running_queries.ex
+++ lib/ecto_mysql_extras/queries/long_running_queries.ex
@@ -36,6 +36,13 @@ defmodule EctoMySQLExtras.LongRunningQueries do
 
     query_db_specific =
       if args[:db] == :mysql do
+        from =
+          if String.starts_with?(args[:version], "5.7.") do
+            "information_schema.PROCESSLIST"
+          else
+            "performance_schema.processlist"
+          end
+
         """
         SELECT
           ID AS `id`,
@@ -45,7 +52,7 @@ defmodule EctoMySQLExtras.LongRunningQueries do
           TIME AS `duration`,
           INFO AS `query`,
           NULL AS `memory_used`
-        FROM performance_schema.processlist
+        FROM #{from}
         WHERE DB = DATABASE()
         AND COMMAND <> 'Sleep'
         AND TIME > #{args[:threshold] / 1000}
